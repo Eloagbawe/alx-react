8df8bc42250ea1032ef05253a693363dbf0c5569
Eloagbawe <eloagbawe@gmail.com>
2023-12-25 23:30:41 +0100

    add assets and css

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,11 +1,12 @@
 import $ from 'jquery';
-import _ from 'lodash'
+import _ from 'lodash';
+import '../css/main.css';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
-$("body").append('Copyright - Holberton School');
+$('body').append('Copyright - Holberton School');
 
 let count = 0;
 
