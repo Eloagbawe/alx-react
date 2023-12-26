3b0bf78439bd3f478a2f4ca1d310dece0568b704
Eloagbawe <eloagbawe@gmail.com>
2023-12-26 23:25:18 +0100

    add file-loader and image-webpack-loader

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -17,6 +17,19 @@ module.exports = {
   module: {
     rules: [
       { test: /\.css$/, use: ['style-loader', 'css-loader']},
+      {
+        test: /\.(gif|png|jp?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
     ],
   },
 };
