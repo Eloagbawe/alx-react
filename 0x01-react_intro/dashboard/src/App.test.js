68b18e2717ec0c4a74236787304eb17a9e99d036
Eloagbawe <eloagbawe@gmail.com>
2024-01-06 23:32:40 +0100

    update app component and tests

--- 0x01-react_intro/dashboard/src/App.test.js
+++ 0x01-react_intro/dashboard/src/App.test.js
@@ -7,8 +7,17 @@ describe('<App/>', () => {
   });
   it('renders a div with the class App-header', () => {
     const app = shallow(<App/>);
-    expect(app.find('header').length).toBe(1);
     expect(app.find('.App-header').length).toBe(1);
-    expect(app.find('header').hasClass('App-header')).toBe(true);
+    expect(app.find('div').at(1).hasClass('App-header')).toBe(true);
+  });
+  it('renders a div with the class App-body', () => {
+    const app = shallow(<App/>);
+    expect(app.find('.App-body').length).toBe(1);
+    expect(app.find('div').at(2).hasClass('App-body')).toBe(true);
+  });
+  it('renders a div with the class App-footer', () => {
+    const app = shallow(<App/>);
+    expect(app.find('.App-footer').length).toBe(1);
+    expect(app.find('div').at(3).hasClass('App-footer')).toBe(true);
   });
 })
